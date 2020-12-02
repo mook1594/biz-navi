69d935de006b3aed4fe7536aacbec7c6ad1406b5
mook1594 <wjdanr89@gmail.com>
2020-12-02 23:32:29 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/mook1594/biznaviapi/BizNaviApiApplication.java
@@ -0,0 +1,13 @@
+package com.mook1594.biznaviapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizNaviApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizNaviApiApplication.class, args);
+	}
+
+}
