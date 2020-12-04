a1ed01c066d207e3b23a79d868b7392921902a72
mook1594 <wjdanr89@gmail.com>
2020-12-04 22:01:42 +0900

    feat: 위도,경도로 거리 구하는 기능 추가 (LocationDistance)

--- src/main/java/com/mook1594/biznaviapi/component/LocationDistance.java
+++ src/main/java/com/mook1594/biznaviapi/component/LocationDistance.java
@@ -2,13 +2,62 @@ package com.mook1594.biznaviapi.component;
 
 import java.math.BigDecimal;
 
+import com.mook1594.biznaviapi.utils.BigDecimalUtils;
+import com.mook1594.biznaviapi.utils.DistanceUtils;
+
 public class LocationDistance {
 
 
-	public LocationDistance(Location fromLocation, Location toLocation) {
+	private final Location fromLocation;
+	private final Location toLocation;
+
+	public LocationDistance(final Location fromLocation, final Location toLocation) {
+		this.fromLocation = fromLocation;
+		this.toLocation = toLocation;
+	}
+
+	public BigDecimal distanceForMile() {
+		return DistanceUtils.radianToDegree(
+			BigDecimalUtils.acos(calculateDistance())
+		).multiply(DistanceUtils.CONSTANT_60)
+			.multiply(DistanceUtils.CONSTANT_1_1515);
 	}
 
 	public BigDecimal distanceForMeter() {
-		return null;
+		return distanceForMile()
+				.multiply(DistanceUtils.MILE_TO_CENTIMETER)
+				.divide(DistanceUtils.UNIT);
+	}
+
+	public BigDecimal distanceForKilometer() {
+		return distanceForMeter()
+				.divide(DistanceUtils.UNIT);
+	}
+
+	private BigDecimal theta(){
+		return fromLocation.getLongitude()
+			.subtract(toLocation.getLongitude());
+	}
+
+	private BigDecimal calculateDistance() {
+		return sinAfterDegreeToRadian(fromLocation.getLatitude())
+			.multiply(sinAfterDegreeToRadian(toLocation.getLatitude()))
+			.add(
+				cosAfterDegreeToRadian(fromLocation.getLatitude())
+					.multiply(cosAfterDegreeToRadian(toLocation.getLatitude()))
+					.multiply(cosAfterDegreeToRadian(theta()))
+			);
+	}
+
+	private BigDecimal sinAfterDegreeToRadian(BigDecimal value) {
+		return BigDecimalUtils.sin(
+			DistanceUtils.degreeToRadian(value)
+		);
+	}
+
+	private BigDecimal cosAfterDegreeToRadian(BigDecimal value) {
+		return BigDecimalUtils.cos(
+			DistanceUtils.degreeToRadian(value)
+		);
 	}
 }
