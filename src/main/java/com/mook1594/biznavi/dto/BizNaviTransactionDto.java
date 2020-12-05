d6aa88a6b1628a9608799dd7575b3443a27d6169
mook1594 <wjdanr89@gmail.com>
2020-12-05 21:09:18 +0900

    feat: 결과 저장 dto 추가

---
+++ src/main/java/com/mook1594/biznavi/dto/BizNaviTransactionDto.java
@@ -0,0 +1,56 @@
+package com.mook1594.biznavi.dto;
+
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.google.common.collect.Lists;
+
+import lombok.Getter;
+
+@Document(collection = "bizTransaction")
+@Getter
+public class BizNaviTransactionDto {
+
+	@Id
+	private String id;
+	private final String stName;
+	private final String glName;
+	private final int dist;
+	private final boolean finish;
+
+	final List<BizNaviLocationInfoDto> locationInfos;
+
+	public BizNaviTransactionDto() {
+		this.id = null;
+		this.stName = null;
+		this.glName = null;
+		this.dist = 0;
+		this.finish = false;
+		this.locationInfos = Lists.newArrayList();
+	}
+
+	/**
+	 * @param transactionId transaction ID
+	 * @param startPlaceName 출발지 명
+	 * @param goalPlaceName 도착지 명
+	 * @param totalDistance 총 거리(m)
+	 * @param transactionFinished transaction 종료 여부
+	 */
+	public BizNaviTransactionDto(
+		final String transactionId,
+		final String startPlaceName,
+		final String goalPlaceName,
+		final int totalDistance,
+		boolean transactionFinished
+	) {
+		this.id = transactionId;
+		this.stName = startPlaceName;
+		this.glName = goalPlaceName;
+		this.dist = totalDistance;
+		this.finish = transactionFinished;
+		this.locationInfos = Lists.newArrayList();
+	}
+}
+
