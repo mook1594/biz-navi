6dab31b4772994d062d11c40877dc448c532a45c
mook1594 <wjdanr89@gmail.com>
2020-12-05 12:29:58 +0900

    refactor: 상수 변경

--- src/main/java/com/mook1594/biznaviapi/service/BizNaviHandler.java
+++ src/main/java/com/mook1594/biznaviapi/service/BizNaviHandler.java
@@ -8,12 +8,15 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class BizNaviHandler {
 
+	private static final int LIMIT_COMPLETE_DISTANCE = 100;
+	private static final int LIMIT_VALID_MOVE = 500;
+
 	public static boolean isArriveGoal(final Location arriveLocation, final Location goalLocation) {
 		LocationDistance distance = new LocationDistance(goalLocation, arriveLocation);
 
 		final int remainDistance = distance.distanceForMeter().intValue();
 		log.info("distanceForMeter: {}", remainDistance);
-		return remainDistance <= 100;
+		return remainDistance <= LIMIT_COMPLETE_DISTANCE;
 	}
 
 	public static boolean isValidMove(Location fromLocation, Location toLocation) {
@@ -21,6 +24,6 @@ public class BizNaviHandler {
 
 		final int moveDistance = distance.distanceForMeter().intValue();
 		log.info("moveForMeter: {}", moveDistance);
-		return moveDistance > 500;
+		return moveDistance > LIMIT_VALID_MOVE;
 	}
 }
