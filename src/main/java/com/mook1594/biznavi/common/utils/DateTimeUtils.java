914a5a4fefe6e2ebe4f543725daab5a0a8cb7fa6
mook1594 <wjdanr89@gmail.com>
2020-12-12 01:30:35 +0900

    feat: DateTimeUtils 추가
    - 시간 계산을 위한 Utils 클래스

---
+++ src/main/java/com/mook1594/biznavi/common/utils/DateTimeUtils.java
@@ -0,0 +1,29 @@
+package com.mook1594.biznavi.common.utils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+
+import com.google.common.base.Strings;
+
+public class DateTimeUtils {
+
+	private static final String DEFAULT_DATETIME_FORMAT = "yyyyMMddHHmmss";
+	private static final int MILLI_SECOND_LENGTH = 3;
+	private static final int MICRO_SECOND_LENGTH = 3;
+
+	public static LocalDateTime toLocalDatetime(final String datetimeString) {
+		int length = DEFAULT_DATETIME_FORMAT.length() + MILLI_SECOND_LENGTH + MICRO_SECOND_LENGTH;
+		final String datetimeStringWithZeroPadding = Strings.padEnd(datetimeString, length, '0');
+		return LocalDateTime.parse(datetimeStringWithZeroPadding, formatter());
+	}
+
+	private static DateTimeFormatter formatter() {
+		return new DateTimeFormatterBuilder()
+			.appendPattern(DEFAULT_DATETIME_FORMAT)
+			.appendValue(ChronoField.MILLI_OF_SECOND, MILLI_SECOND_LENGTH)
+			.appendValue(ChronoField.MICRO_OF_SECOND, MICRO_SECOND_LENGTH)
+			.toFormatter();
+	}
+}
---
+++ src/test/java/com/mook1594/biznavi/common/utils/DateTimeUtilsTest.java
@@ -0,0 +1,43 @@
+package com.mook1594.biznavi.common.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class DateTimeUtilsTest {
+
+	private final String DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss.SSSS";
+
+	@ParameterizedTest
+	@DisplayName("string을 LocalDatetime으로 변환")
+	@CsvSource(delimiter = ',', value = {
+		"202011131101020001,2020-11-13 11:01:02.0001",
+		"202012010402500002,2020-12-01 04:02:50.0002"
+	})
+	public void toLocalDatetime(final String datetimeString, final String expected) {
+		final String string1 = datetimeString;
+		LocalDateTime localDateTime = DateTimeUtils.toLocalDatetime(string1);
+		assertEquals(expected, localDateTime.format(DateTimeFormatter.ofPattern(DATETIME_FORMAT)));
+	}
+
+	@ParameterizedTest
+	@DisplayName("시간차이 확인")
+	@CsvSource(delimiter = ',', value = {
+		"202011131101020001,202011131201020002, 1",
+		"202012010402500002,202012011002500002, 6"
+	})
+	public void timespan(final String datetimeString1, final String datetimeString2, final int hour) {
+		LocalDateTime localDateTime1 = DateTimeUtils.toLocalDatetime(datetimeString1);
+		LocalDateTime localDateTime2 = DateTimeUtils.toLocalDatetime(datetimeString2);
+
+		Duration duration = Duration.between(localDateTime1, localDateTime2);
+
+		assertEquals(hour, duration.getSeconds() / 3600);
+	}
+}
