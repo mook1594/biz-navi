b87a9d89f89a8f3161650cde9888dcee3a5057c9
mook1594 <wjdanr89@gmail.com>
2020-12-12 10:20:22 +0900

    feat: 도작치 위,경도 추가

--- src/main/java/com/mook1594/biznavi/transactions/dto/BizNaviTransactionDto.java
+++ src/main/java/com/mook1594/biznavi/transactions/dto/BizNaviTransactionDto.java
@@ -6,6 +6,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.google.common.collect.Lists;
+import com.mook1594.biznavi.common.model.Location;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -19,6 +20,8 @@ public class BizNaviTransactionDto {
 	private final String stName;
 	private final String glName;
 	private final double dist;
+	private final String glLat;
+	private final String glLng;
 	private final boolean finish;
 
 	final List<BizNaviLocationInfoDto> locationInfos;
@@ -36,12 +39,16 @@ public class BizNaviTransactionDto {
 		final String startPlaceName,
 		final String goalPlaceName,
 		final double totalDistance,
-		boolean transactionFinished
+		final String goalLatitude,
+		final String goalLongitude,
+		final boolean transactionFinished
 	) {
 		this.id = transactionId;
 		this.stName = startPlaceName;
 		this.glName = goalPlaceName;
 		this.dist = totalDistance;
+		this.glLat = goalLatitude;
+		this.glLng = goalLongitude;
 		this.finish = transactionFinished;
 		this.locationInfos = Lists.newArrayList();
 	}
@@ -67,6 +74,20 @@ public class BizNaviTransactionDto {
 
 	public void addLocationInfo(BizNaviLocationInfoDto dto) {
 		locationInfos.add(dto);
+	}
+
+	public Location getGoalLocation() {
+		return new Location(glLat, glLng);
+	}
+
+	public Location getLastLocation() {
+		final int lastIndex = locationInfos.size() - 1;
+		return locationInfos.get(lastIndex).getLocation();
+	}
+
+	public String getLastDatetime() {
+		final int lastIndex = locationInfos.size() - 1;
+		return locationInfos.get(lastIndex).getDatetime();
 	}
 }
 
